733a91b7913bb82570768b5e9215d5b8d4d0675e
404NotYukariFound <1224615301@qq.com>
2020-12-27 09:30:22 +0800

    添加提示语句，改动逻辑错误

--- src/CmdShop.java
+++ src/CmdShop.java
@@ -10,16 +10,17 @@ public class CmdShop {
         String username;
         String password;
         Scanner sc=new Scanner(System.in);
+        System.out.println("请输入用户名");
         username=sc.next();
+        System.out.println("请输入密码");
         password=sc.next();
-
         for (int i=0;i<users.length;i++)
         {
             if(users[i].getUsername().equals(username)&&users[i].getPassword().equals(password))
             {
                 System.out.println("登陆成功");
             }
-            else if(i==users.length)
+            else if(i==users.length-1)
             {
                 System.out.println("用户名或密码错误");
             }
